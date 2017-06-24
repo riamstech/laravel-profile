66a210f965e4f0a1eaa4df6c73c8223d406483e7
berkayoztunnc <mailberkayoztunc@gmail.com>
2017-06-24 16:14:39 +0300

    new api and testing stracture

---
+++ test/unit/DataBaseTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Berkayoztunc\LaravelProfile\Test\Unit;
+
+
+use Berkayoztunc\LaravelProfile\Test\TestCase;
+use Illuminate\Support\Facades\Schema;
+
+class DataBaseTest extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->artisan('migrate', ['--database' => 'testing']);
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+    }
+
+    public function test_user_update_api()
+    {
+        $this->assertTrue(Schema::hasColumn('users', 'about'));
+    }
+}
